173caaee23c87bd463861843f35deaff5976b4bf
Bankole Ebenezer <bankoleebenezer111@gmail.com>
2026-04-03 13:57:30 -0700

    commit and push all changes to github

--- swagger/swagger.js
+++ swagger/swagger.js
@@ -7,6 +7,16 @@ const doc = {
   },
   host: 'localhost:8080',
   schemes: ['http', 'https'],
+  securityDefinitions: {
+    github_auth: {
+      type: 'oauth2',
+      authorizationUrl: 'https://github.com/login/oauth/authorize',
+      flow: 'implicit',
+      scopes: {
+        user: 'read profile'
+      }
+    }
+  }
 };
 
 const outputFile = './swagger/swagger.json';
